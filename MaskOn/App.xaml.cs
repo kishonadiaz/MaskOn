34f14e3e94addd608d9ff72fcdb2a2a2a55f4d4a
Kishon Diaz <simplecast@hotmail.com>
2025-12-18 07:08:45 -0500

    Add project files.

---
+++ MaskOn/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MaskOn
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
